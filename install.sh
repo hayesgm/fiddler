c327d33af6d92f316db828a98f27da847505a9ac
Geoff Hayes <hayesgm@gmail.com>
2013-09-03 00:19:28 -0700

    add check for sudo

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
+echo "We will need sudo to write systemd service"
+sudo echo "Continuing installation"
+
 cd ~
-wget https://github.com/hayesgm/fiddler/raw/master/bin/fiddler.linux.tar.gz && tar -xvf fiddler.linux.tar.gz && ./fiddler.linux -i -c $1
+wget https://github.com/hayesgm/fiddler/raw/master/bin/fiddler.linux.tar.gz && tar -xvf fiddler.linux.tar.gz && sudo ./fiddler.linux -i -c $1
