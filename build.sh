df8992e5f22d6290b55d55af5eaceba3c541c209
Geoff Hayes <hayesgm@gmail.com>
2013-09-02 21:01:48 -0700

    Change build structure

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-go build fiddler.go
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o fiddler.linux fiddler.go
+go build -o bin/fiddler fiddler.go
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bin/fiddler.linux fiddler.go
+tar -czvf bin/fiddler.tar.gz bin/fiddler
+tar -czvf bin/fiddler.linux.tar.gz bin/fiddler.linux
