ca560db9fe0ee0bca456b512d1bae8af79a600c9
Geoff Hayes <hayesgm@gmail.com>
2013-09-02 21:18:22 -0700

    update launcher

--- launcher/launcher.go
+++ launcher/launcher.go
@@ -6,6 +6,7 @@ import (
   "os/exec"
 )
 
+// TODO: which docker
 func Launch(docker *config.DockerConf) (cmd *exec.Cmd, err error) {
   log.Printf("Launching %#v", docker)
   
@@ -15,7 +16,7 @@ func Launch(docker *config.DockerConf) (cmd *exec.Cmd, err error) {
   for i := 0; i < len(docker.Args); i++ {
     args[i+2] = docker.Args[i]
   }
-  cmd = &exec.Cmd{Path: "docker", Args: args}
+  cmd = &exec.Cmd{Path: "/usr/bin/docker", Args: args}
   
   err = cmd.Start()
   if err != nil {
