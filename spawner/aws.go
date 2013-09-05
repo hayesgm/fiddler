11fc2c243ef8ea81f4d0aa36dec2d8417932311a
Geoff Hayes <hayesgm@gmail.com>
2013-09-05 02:06:45 -0700

    start to get the full spawn environment running

--- spawner/aws.go
+++ spawner/aws.go
@@ -4,16 +4,32 @@ import (
   "log"
   "launchpad.net/goamz/aws"
   "launchpad.net/goamz/ec2"
+  // "github.com/bmizerany/aws"
   "errors"
+  "fmt"
 )
 
 type AmazonSpawnPool struct {
   // Here we will find anything necessary
   auth aws.Auth
+  zone aws.Region
+  name string
+  privateKey string
+  sg ec2.SecurityGroup
 }
 
-func NewAmazonSpawnPool(accessKey, secretKey string) (pool AmazonSpawnPool) {
-  pool = AmazonSpawnPool{auth: aws.Auth{AccessKey: accessKey, SecretKey: secretKey}}
+func NewAmazonSpawnPool(name, accessKey, secretKey, zone, privateKey string) (pool *AmazonSpawnPool, err error) {
+  var awsZone, ok = aws.Regions[zone]
+  if !ok {
+    return nil, errors.New(fmt.Sprintf("Unable to find zone: %s", zone))
+  }
+
+  pool = &AmazonSpawnPool{name: name, auth: aws.Auth{AccessKey: accessKey, SecretKey: secretKey}, zone: awsZone, privateKey: privateKey}
+  
+  pool.sg, err = pool.findOrCreateSecurityGroup()
+  if err != nil {
+    return
+  }
 
   return
 }
@@ -26,13 +42,16 @@ func NewAmazonSpawnPool(accessKey, secretKey string) (pool AmazonSpawnPool) {
 // These will be sent to the environment for Fiddler
 
 // Spawn instance will spawn an instance to whatever stack is specified in the environment
-func (pool AmazonSpawnPool) Grow() (err error) {
+func (pool AmazonSpawnPool) Grow(conf string) (err error) {
   
   e := ec2.New(pool.auth, aws.USEast)
 
   options := ec2.RunInstances{
     ImageId: "ami-5b632e32", // CoreOS
     InstanceType: "t1.micro",
+    KeyName: "fiddler", // TODO: We should add this
+    SecurityGroups: []ec2.SecurityGroup{pool.sg},
+    UserData: []byte(fmt.Sprintf("https://fiddler/%s/%s", pool.sg.Id, pool.name)), // We need something consistent here
   }
 
   resp, err := e.RunInstances(&options)
@@ -42,9 +61,15 @@ func (pool AmazonSpawnPool) Grow() (err error) {
 
   for _, instance := range resp.Instances {
     log.Println("Now running", instance.InstanceId)
+    // Next, we'll need to install Fiddler
+    // It's kind of annoying, but we can ssh into our new box
+    installCmd := fmt.Sprintf("\\curl -L https://raw.github.com/hayesgm/fiddler/master/install.sh | bash -s %s", conf)
+    err = sshCommand(instance.IPAddress, "core", pool.privateKey, installCmd)
+    if err != nil {
+      return err
+    }
   }
 
-  // Next, we'll need to install Fiddler
   return
 }
 
@@ -54,4 +79,73 @@ func (pool AmazonSpawnPool) Shrink() (err error) {
   // Doesn't matter if it's the leader, but it might be nice to avoid that
 
   return errors.New("Not implemented")
+}
+
+// This will set-up a new spawn pool, if it doesn't exist
+// For AWS, we'll track this by security groups
+func (pool *AmazonSpawnPool) findOrCreateSecurityGroup() (ec2.SecurityGroup, error) {
+  sgName := fmt.Sprintf("fiddler-%s", pool.name)
+  groups := []ec2.SecurityGroup{ec2.SecurityGroup{Name: sgName}}
+  
+  e := ec2.New(pool.auth, aws.USEast)
+
+  sgResp, sgErr := e.SecurityGroups(groups, nil)
+  if sgErr == nil { // We found the security group
+    return sgResp.Groups[0].SecurityGroup, nil
+  } else {
+    log.Println("Creating security group:", pool.name)
+    resp, err := e.CreateSecurityGroup(sgName, fmt.Sprintf("Fiddler env for %s", sgName))
+    if err != nil {
+      return ec2.SecurityGroup{}, err
+    }
+
+    // For now, we're going to open up 4001/7001 to the group
+    // and ports 21, 80 to the world
+    // This should have a config option
+    sg := resp.SecurityGroup
+
+    openPorts := []int{22, 80}
+    perms := make([]ec2.IPPerm, 2+len(openPorts))
+    perms[0] = ec2.IPPerm{Protocol: "tcp", FromPort: 4001, ToPort: 4001, SourceGroups: []ec2.UserSecurityGroup{ec2.UserSecurityGroup{Id: sg.Id}}}
+    perms[1] = ec2.IPPerm{Protocol: "tcp", FromPort: 7001, ToPort: 7001, SourceGroups: []ec2.UserSecurityGroup{ec2.UserSecurityGroup{Id: sg.Id}}}
+    for i, port := range openPorts {
+      perms[i+2] = ec2.IPPerm{Protocol: "tcp", FromPort: port, ToPort: port, SourceIPs: []string{"0.0.0.0/0"}}
+    }
+
+    _, err = e.AuthorizeSecurityGroup(sg, perms)
+    if err != nil {
+      // Delete unauthorized SG?
+      return ec2.SecurityGroup{}, err
+    }
+
+    return sg, err
+  }
+
+/*
+  func (pool *AmazonSpawnPool) findOrCreateKeyPair() (err error) {
+    keyName := fmt.Sprintf("fiddler-%s", pool.name)
+
+    r := aws.TemplateRequest // Make a copy of the template request
+
+    // Set the Action parameter
+    r.Add("Action", "DescribeKeyPairs")
+
+    type DescribeKeyPairsResponse struct {
+      aws.Header
+      Keys           []Key `xml:"keySet>item"`
+    }
+
+    type Key struct {
+      KeyName        string
+      KeyFingerprint string
+    }
+
+    v := new(DescribeKeyPairsResponse)
+    aws.Do(r, v)
+
+    for _, key := range v.Keys {
+      if KeyName == 
+    }
+  }
+*/
 }
--- spawner/spawner.go
+++ spawner/spawner.go
@@ -9,15 +9,18 @@ import (
   "fmt"
   "os"
   "errors"
+  "io/ioutil"
 )
 
 type SpawnPool interface {
-  Grow() (err error)
+  // Grows the pool by one instance
+  Grow(conf string) (err error)
+  // Shrinsk the pool one instance
   Shrink() (err error)
 }
 
 // GetSpawnPool will return spawn pool from the environment or etcd
-func GetSpawnPool() (pool SpawnPool, err error) {
+func GetSpawnPool(name string) (pool SpawnPool, err error) {
   // First, we'll try to grab the environment from env
   if len(os.Getenv("cloud")) == 0 {
     return nil, errors.New("Must provide cloud env variable to run spawner")
@@ -26,12 +29,23 @@ func GetSpawnPool() (pool SpawnPool, err error) {
   // We have a cloud environment variable
   switch os.Getenv("cloud") {
   case "aws":
-    var key, secret string
-    key, secret = os.Getenv("key"), os.Getenv("secret")
-    if len(key) == 0 || len(secret) == 0 {
-      return nil, errors.New("Must provide key and secret to spawn AWS instances")
+    var key, secret, zone string
+    key, secret, zone = os.Getenv("key"), os.Getenv("secret"), os.Getenv("zone")
+    if len(key) == 0 || len(secret) == 0 || len(zone) == 0 {
+      return nil, errors.New("Must provide key, secret and zone to spawn AWS instances")
     }
-    return NewAmazonSpawnPool(key, secret), nil
+    // There's hacks all around for this
+    privateKeyFile := os.Getenv("privateKeyFile")
+    if len(privateKeyFile) == 0 {
+      return nil, errors.New("Must provide privateKey")
+    }
+    
+    privateKey, err := ioutil.ReadFile(privateKeyFile)
+    if err != nil {
+      return nil, err
+    }
+
+    return NewAmazonSpawnPool(name, key, secret, zone, string(privateKey))
   default:
     return nil, errors.New(fmt.Sprintf("Unknown cloud: %s", os.Getenv("cloud")))
   }
